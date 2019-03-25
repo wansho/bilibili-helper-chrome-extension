88c7ca458e40f1cf5414f17e88954b38c8d7ba00
wansho <wanshojs@hotmail.com>
2019-03-25 21:23:01 +0800

    给按钮加入点击事件

--- contentscript.js
+++ contentscript.js
@@ -5,6 +5,18 @@
 
 let url = document.documentURI;
 let cookie = document.cookie;
+video_info = {"url": url, "cookie": cookie};
 
 // 发送 messge 给 background.js，将 url 和 cookie 交给 background.js 处理
-chrome.runtime.sendMessage({command: {"url": url, "cookie": cookie}});
+chrome.runtime.sendMessage({command: video_info});
+
+// 接收 contentscript.js 和 popup.js发送过来的 html 文件进行分析
+chrome.extension.onMessage.addListener(
+	function(request, sender, sendResponse) {
+	// background.js 中的log时在扩展页面该扩展的背景页显示的
+		command = request.command;
+		if(command == "start"){ //收到 popup.js 发来的点击事件detect后，向background.js发送检测请求
+			chrome.runtime.sendMessage({command: video_info});
+		}
+	}
+);
