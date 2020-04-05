f4579579222fdeaa02b87ab114495f0941424c4d
wansho <wanshojs@hotmail.com>
2020-04-05 20:52:44 +0800

    兼容 BV

--- contentscript.js
+++ contentscript.js
@@ -1,6 +1,6 @@
 // Content Script 用来和 前端页面 通过 DOM 进行交互，会在加载前端的时候自动执行，
 // Content Script 已经在 manifest 中设置了 在页面加载完成后执行
-// Content Script 中不支持 console.log() 方法
+// Content Script 中的 log 会打印在其操作的页面上，也就是 Web page 上，F12
 // Content Script 只限于 DOM 操作，做 html，css，js 的相关操作，不能完成与服务器交互的操作，事实上，Google 对 Content Script 的限制非常严格，因为 Content-Script 可以轻而易举的获取到用户的隐私
 
 progress_width = 0;
